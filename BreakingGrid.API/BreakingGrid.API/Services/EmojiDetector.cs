8a0c704681e3836adf5020a0c75616edd0fe4b6e
Artak Galstyan <artak.galstyan@picsart.com>
2022-02-25 12:09:26 +0400

    Created empty services

---
+++ BreakingGrid.API/BreakingGrid.API/Services/EmojiDetector.cs
@@ -0,0 +1,21 @@
+using BreakingGrid.API.Abstractions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreakingGrid.API.Services
+{
+    public class EmojiDetector : IEmojiDetector
+    {
+    }
+}
+
